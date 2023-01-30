bdaae96166c77abe91b738c61bae763598449566
지수민 <wltn1873@gmail.com>
2023-01-31 01:33:26 +0900

    feat: 프로젝트 경로별 이미지 추가 및 설정

---
+++ src/constants/path.ts
@@ -0,0 +1,8 @@
+const PATH = {
+  MAIN: { NAME: '혼자어때', URL: '/' },
+  AREA: { NAME: '지역관광지', URL: '/area' },
+  LOCATION: { NAME: '내주변관광지', URL: '/location' },
+  COURSE: { NAME: '나홀로여행코스', URL: '/course' },
+};
+
+export default PATH;
