23f88f6b0ffa284fc03c7d59ca464a087c812fd2
지수민 <wltn1873@gmail.com>
2023-02-01 01:23:16 +0900

    fix: type any 수정

--- src/hooks/queries/search.ts
+++ src/hooks/queries/search.ts
@@ -3,20 +3,34 @@ import queryKey from '@/constants/queryKey';
 import { useQuery } from '@tanstack/react-query';
 import { AxiosResponse } from 'axios';
 
+interface SearchKeywordResponse {
+  contentid: string;
+  contenttypeid: string;
+  firstimage: string;
+  title: string;
+}
+
 const useSearchKeyword = (keyword?: string) => {
   return useQuery([queryKey.KEYWORD], () => searchApi.searchKeyword(keyword), {
     retry: 0,
     enabled: !!keyword,
     select: (data: AxiosResponse) => {
       const result = data.data.response.body;
-      const items = result.items.item.map((data: any) => {
-        return {
-          contentId: data.contentid,
-          contentTypeId: data.contenttypeid,
-          image: data.firstimage,
-          title: data.title,
-        };
-      });
+      const items = result.items.item.map(
+        ({
+          contentid,
+          contenttypeid,
+          firstimage,
+          title,
+        }: SearchKeywordResponse) => {
+          return {
+            contentId: contentid,
+            contentTypeId: contenttypeid,
+            image: firstimage,
+            title: title,
+          };
+        },
+      );
       return {
         totalCount: result.totalCount,
         items,
