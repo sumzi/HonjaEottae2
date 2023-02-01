ac9425f75bd505d58453192dcebdf822bb9c0743
지수민 <wltn1873@gmail.com>
2023-02-01 22:01:55 +0900

    style: 메인페이지 메뉴 스타일 수정

--- src/components/common/Layout/Layout.styles.ts
+++ src/components/common/Layout/Layout.styles.ts
@@ -3,9 +3,10 @@ import { css, Theme } from '@emotion/react';
 const layout = ({ sizes }: Theme) => css`
   max-width: 1024px;
   margin: 0 auto;
-  padding-top: 14rem;
+  padding-top: 20rem;
   ${sizes.tablet} {
-    padding: 14rem 10rem;
+    padding-left: 10rem;
+    padding-right: 10rem;
   }
 `;
 
