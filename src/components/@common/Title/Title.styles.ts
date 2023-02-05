75860a3b2e9d61bdc2212b598c94afd41352c8f1
지수민 <wltn1873@gmail.com>
2023-02-05 23:36:56 +0900

    style: 스타일 변경

--- src/components/@common/Title/Title.styles.ts
+++ src/components/@common/Title/Title.styles.ts
@@ -3,11 +3,14 @@ import { css, Theme } from '@emotion/react';
 const wrapper = ({ flex, colors }: Theme) => css`
   ${flex.column}
   padding: 10rem;
-  & > h1 {
+  h1 {
     font-size: 6.5rem;
     margin-bottom: 2rem;
+    label {
+      color: ${colors.PRIMARY};
+    }
   }
-  & > p {
+  p {
     font-size: 4rem;
     color: ${colors.GRAY_500};
   }
