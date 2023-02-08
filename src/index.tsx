114861b47d90d2d746d1ade39cd3f6dc5b49b000
지수민 <wltn1873@gmail.com>
2023-02-08 14:27:28 +0900

    feat: error 처리 및 react lazy 적용

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from '@emotion/react';
@@ -6,6 +6,8 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import App from '@/App';
 import GlobalStyle from '@/styles/GlobalStyle';
 import theme from '@/styles/theme';
+import ErrorBoundary from '@/components/@common/ErrorBoundary/ErrorBoundary';
+import Error from '@/pages/Error';
 
 const rootNode = document.getElementById('root') as Element;
 
@@ -25,7 +27,11 @@ ReactDOM.createRoot(rootNode).render(
       <BrowserRouter>
         <QueryClientProvider client={queryClient}>
           <GlobalStyle />
-          <App />
+          <Suspense>
+            <ErrorBoundary fallback={<Error />}>
+              <App />
+            </ErrorBoundary>
+          </Suspense>
         </QueryClientProvider>
       </BrowserRouter>
     </ThemeProvider>
---
+++ src/pages/Error.tsx
@@ -0,0 +1,32 @@
+import { Link } from 'react-router-dom';
+import { css, Theme } from '@emotion/react';
+import Button from '@/components/@common/Button/Button';
+import Layout from '@/components/@common/Layout/Layout';
+import PATH from '@/constants/path';
+
+const wrapper = ({ flex }: Theme) => css`
+  ${flex.column}
+  height:80vh;
+  img {
+    height: 40rem;
+  }
+  p {
+    margin: 8rem 0;
+    font-size: 5rem;
+  }
+`;
+
+function Error() {
+  return (
+    <Layout>
+      <div css={wrapper}>
+        <p>에러가 발생했습니다. 잠시 후에 다시 시도해주세요.</p>
+        <Link to={PATH.MAIN.URL}>
+          <Button>혼자어때 페이지로 돌아가기</Button>
+        </Link>
+      </div>
+    </Layout>
+  );
+}
+
+export default Error;
