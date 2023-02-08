2e2dc28eef4c8b0b63e03db6198a6fe947e5a415
지수민 <wltn1873@gmail.com>
2023-02-08 11:59:01 +0900

    feat: 위치 관련 커스텀 훅 구현

--- src/hooks/useKakaoMap.ts
+++ src/hooks/useKakaoMap.ts
@@ -1,5 +1,6 @@
 import { useEffect, useRef } from 'react';
 import { LocationType } from '@/types/detail';
+import { KaKaoMapType, LocationBasedListType } from '@/types/location';
 
 declare global {
   interface Window {
@@ -7,23 +8,41 @@ declare global {
   }
 }
 
-const useKakaoMap = ({ mapX, mapY }: LocationType) => {
+const useKakaoMap = ({ mapX, mapY, data }: KaKaoMapType) => {
   const { kakao } = window;
   const container = useRef(null);
 
   useEffect(() => {
-    const position = new kakao.maps.LatLng(mapY, mapX);
-    const options = {
-      center: position,
-      level: 4,
-    };
-    const map = new kakao.maps.Map(container.current, options);
-    map.setDraggable(false);
-    map.setZoomable(false);
-    const marker = new kakao.maps.Marker({
-      position,
-    });
-    marker.setMap(map);
+    const position = new kakao.maps.LatLng(mapX, mapY);
+
+    if (data) {
+      const options = {
+        center: position,
+        level: 6,
+      };
+      const map = new kakao.maps.Map(container.current, options);
+      map.setDraggable(true);
+      map.setZoomable(true);
+
+      data.forEach((item, i) => {
+        const marker = new kakao.maps.Marker({
+          map: map,
+          position: new kakao.maps.LatLng(item.mapX, item.mapY),
+        });
+      });
+    } else {
+      const options = {
+        center: position,
+        level: 4,
+      };
+      const map = new kakao.maps.Map(container.current, options);
+      map.setDraggable(false);
+      map.setZoomable(false);
+      const marker = new kakao.maps.Marker({
+        position,
+      });
+      marker.setMap(map);
+    }
   }, []);
 
   return { container };
